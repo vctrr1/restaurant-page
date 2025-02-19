1c31d2a06a52073a40a4c8220a37961b9f66fb17
vctrr1 <victoremaanoel@outlook.com>
2025-02-19 18:11:19 -0300

    add products page and install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>Test</div>;
-}
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Button>Test</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,5 @@
+const ProductPage = () => {
+  return <div>Products Page</div>;
+};
+
+export default ProductPage;
