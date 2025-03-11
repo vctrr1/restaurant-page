fea9b8e31b14a63e801c289d6c24d75bce9a0501
vctrr1 <victoremaanoel@outlook.com>
2025-03-10 21:43:10 -0300

    chore: install some compoenets from shadcn

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "img.freepik.com" }],
+    domains: ["u9a6wmr3as.ufs.sh", "png.pngtree.com"],
   },
 };
 
