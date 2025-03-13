8bc460dd3afd0e30556c1b6270b279f890572c7f
vctrr1 <victoremaanoel@outlook.com>
2025-03-13 10:34:41 -0300

    feat: ensure cart button is disabled if cart is empty

--- src/app/[slug]/menu/components/cart-sheet.tsx
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 
+import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 
 import { CartContext } from "../contexts/cart";
@@ -8,6 +9,10 @@ import CartProductItem from "./cart-product-item";
 const CartSheet = () => {
 
     const {isOpen, toggleCart, products} = useContext(CartContext)
+
+    const cartIsEmpty = () => {
+        return products.length <= 0;
+    }
    
     return (
         <Sheet open={isOpen} onOpenChange={toggleCart}>
@@ -15,10 +20,18 @@ const CartSheet = () => {
                 <SheetHeader>
                     <SheetTitle>Carrinho</SheetTitle>
                 </SheetHeader>
-                {products.map(product => (
-                    <CartProductItem key={product.id} product={product}/>
-                ))}
+                <div className="flex flex-col h-full py-5">
+                    <div className="flex-auto">
+                        {products.map(product => (
+                            <CartProductItem key={product.id} product={product}/>
+                        ))}
+                    </div>
+                    <Button className="w-full h-8 rounded-full" disabled={cartIsEmpty()}>
+                        Finalizar pedido
+                    </Button>
+                </div>
             </SheetContent>
+
         </Sheet>
     );
 }
