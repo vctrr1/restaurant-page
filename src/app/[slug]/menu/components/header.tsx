3f93066e702a591d9ba3335f481ba2921463310e
vctrr1 <victoremaanoel@outlook.com>
2025-03-13 10:35:04 -0300

    feat: make sure cart sheet open on the restaurant page (menu)

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -10,6 +10,7 @@ import { useContext } from "react";
 import { Button } from "@/components/ui/button";
 
 import { CartContext } from "../contexts/cart";
+import CartSheet from "./cart-sheet";
 
 interface RestaurantHeaderProps {
   restaurant: Pick<Restaurant, "coverImageUrl" | "name">;
@@ -26,30 +27,34 @@ const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
   const router = useRouter();
 
   return (
-    <div className="relative h-[250px] w-full">
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute left-4 top-4 z-50 rounded-full"
-        onClick={() => router.back()}
-      >
-        <ChevronLeftIcon />
-      </Button>
-      <Image
-        alt={restaurant.name}
-        fill
-        src={restaurant.coverImageUrl}
-        className="object-cover"
-      />
-      <Button
-        variant="secondary"
-        size="icon"
-        className="absolute right-4 top-4 z-50 rounded-full"
-        onClick={handleAddToCart}
-      >
-        <ScrollTextIcon />
-      </Button>
-    </div>
+    <>
+      <div className="relative h-[250px] w-full">
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute left-4 top-4 z-50 rounded-full"
+          onClick={() => router.back()}
+        >
+          <ChevronLeftIcon />
+        </Button>
+        <Image
+          alt={restaurant.name}
+          fill
+          src={restaurant.coverImageUrl}
+          className="object-cover"
+        />
+        <Button
+          variant="secondary"
+          size="icon"
+          className="absolute right-4 top-4 z-50 rounded-full"
+          onClick={handleAddToCart}
+        >
+          <ScrollTextIcon />
+        </Button>
+      </div>
+      <CartSheet/>
+    </>
+
   );
 };
 
